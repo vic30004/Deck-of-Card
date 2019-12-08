5cd2be631e7cb776d7d05fa4793d8600c745a3a9
vic30004 <victor_993@msn.com>
2019-12-08 13:52:00 -0800

    added two more test to make sure the items are added correctly to the the slice

--- deck_test.go
+++ deck_test.go
@@ -10,4 +10,12 @@ func TestNewDeck(t *testing.T) {
 	if len(d) != 52 {
 		t.Errorf("Expected deck length of 52, but instead got %v", len(d))
 	}
+
+	if d[0] != "Ace of Spades" {
+		t.Errorf("Expected first card to be Ace of Spades, but instead got %v", d[0])
+	}
+
+	if d[len(d)-1] != "King of Clubs" {
+		t.Errorf("Expected last card to be King of Clubs, but instead got %v", d[len(d)-1])
+	}
 }
