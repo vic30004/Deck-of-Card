be029e59e9b2dc97964b951c94fce7a3c64064dc
vic30004 <victor_993@msn.com>
2019-12-08 10:46:49 -0800

    Added function to create a new deck

--- deck.go
+++ deck.go
@@ -6,9 +6,25 @@ import "fmt"
 
 type deck []string
 
-func (d deck) print(){
-	for i, card := range d{
-		fmt.Println(i,card)
+func newDeck() deck {
+	cards := deck{}
+
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"}
+
+	for _, cardS := range cardSuits {
+		for _, cardV := range cardValues {
+			cards = append(cards, cardV+" of "+cardS)
+
+		}
 	}
-	
+
+	return cards
+}
+
+func (d deck) print() {
+	for i, card := range d {
+		fmt.Println(i, card)
+	}
+
 }
--- main.go
+++ main.go
@@ -2,14 +2,14 @@ package main
 
 import "fmt"
 
+
+
+
 func main() {
-	cards := deck{newCard(),newCard()}
-	cards = append(cards, newCard())
+	cards := newDeck()
+
 	cards.print()
 	fmt.Printf("%T", cards)
+
 }
 
-func newCard() string {
-
-	return "Five of Diamonds"
-}
