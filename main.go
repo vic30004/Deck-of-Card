b296dfdfbff0e60e94dc29e7eb976d85815232b2
vic30004 <victor_993@msn.com>
2019-12-08 13:08:46 -0800

    started working on shuffle function

--- main.go
+++ main.go
@@ -1,13 +1,8 @@
 package main
 
-import "fmt"
 
 func main() {
-	cards := newDeck()
-	cards.saveToFile("New Deck")
-	fmt.Println(newDeckFromFile("New Deck"))
-	hand, remainigDeck := deal(cards, 5)
-	hand.print()
-	remainigDeck.print()
-
+cards:= newDeck()
+cards.shuffle()
+cards.print()
 }
